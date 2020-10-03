ca61be63690f8d0bc25a44705bda648e41292490
jithinaji <jithin_aji@outlook.com>
2020-10-03 20:14:33 +0530

    about page and gallery page

--- public/js/sahil.js
+++ public/js/sahil.js
@@ -2,9 +2,26 @@ var app = new Vue({
   el: "#mainPage",
   data: {
     showIntro: true,
+    showContact: false,
+    showGallery: false,
   },
   methods: {
-    showGallery: function () {},
-    showContact: function () {},
+    clickGallery: function () {
+      this.showIntro = false;
+      this.showContact = false;
+      this.showGallery = true;
+      let body = document.getElementsByTagName("body");
+      body.classList.remove("lg:overflow-hidden");
+    },
+    clickContact: function () {
+      this.showIntro = false;
+      this.showContact = true;
+      this.showGallery = false;
+    },
+    clickBack: function () {
+      this.showIntro = true;
+      this.showContact = false;
+      this.showGallery = false;
+    },
   },
 });
